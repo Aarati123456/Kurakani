4169259e3cda3de5a44bd11fd4ebfa7208ca2cef
Aarati123456 <aaratiangbuhang2058@gmail.com>
2025-07-23 10:51:35 +0545

    Models and forms completed

--- kura/views.py
+++ kura/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
+def home(request):
+    return render(request, '')
+
+def register_view(request):
+    if request.method=='POST':
+        username= request.POST['username']
+        passdword= request.POST['PASSWORD']
+        
+        
+        if User.objects.filter(Username=Username).exist():
+           messages.error(request, "Username already exist")
+    
+    
+    
